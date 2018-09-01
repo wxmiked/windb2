97fb88aa9ae8ded6a742a9f87a54f08f5b65dd35
Mike Dvorak <miked@saildrone.com>
2018-08-31 17:40:13 -0700

    WRF coordinates are not ingested in their native LCC coordinate

--- windb2/model/wrf/wrf.py
+++ windb2/model/wrf/wrf.py
@@ -15,6 +15,7 @@ import logging
 # Set up logging for InsertAbstract
 logger = logging.getLogger('windb2')
 
+
 def create_wrf_srid(windb2_instance, ncfile):
     """Create a new SRID for this domain based on a spherical earth.
 
@@ -33,13 +34,23 @@ def create_wrf_srid(windb2_instance, ncfile):
     # Create the SRID insert string.  Some nice person documented this for WRF here:
     # http://spatialreference.org/ref/sr-org/wrf-sphere/
     srid_sql = "INSERT INTO \"spatial_ref_sys\" (srid, auth_name, auth_srid, srtext, proj4text) VALUES (" + str(new_srid) + \
-            ", 'WRF '," + str(new_srid) + "," + \
-            """'PROJCS["SPHERICAL WRF", GEOGCS["GCS_Sphere_WRF",
-            DATUM["Sphere_WRF",
-            SPHEROID["Sphere_WRF",6370000.0,0.0]],
-            PRIMEM["Greenwich",0.0],
-            AUTHORITY["EPSG","8901"]]',""" + \
-            "'+proj=longlat +a=6370000 +b=6370000 +no_defs') RETURNING srid"
+               ", 'WRF '," + str(new_srid) + "," + \
+               """'PROJCS["WRF Lambert",
+                    GEOGCS["unnamed ellipse",
+                        DATUM["unknown",
+                            SPHEROID["unnamed",6370000,0]],
+                        PRIMEM["Greenwich",0],
+                        UNIT["degree",0.0174532925199433]],
+                    PROJECTION["Lambert_Conformal_Conic_2SP"],
+                    PARAMETER["standard_parallel_1",{}],
+                    PARAMETER["standard_parallel_2",{}],
+                    PARAMETER["latitude_of_origin",{}],
+                    PARAMETER["central_meridian",{}],
+                    PARAMETER["false_easting",0],
+                    PARAMETER["false_northing",0],
+                    UNIT["Meter",1]]',""".format(ncfile['WRF'].TRUELAT1, ncfile['WRF'].TRUELAT2, ncfile['WRF'].CEN_LAT, ncfile['WRF'].STAND_LON) + \
+               "'+proj=lcc +lat_1={} +lat_2={} +lat_0={} +lon_0={} +x_0=0 +y_0=0 +a=6370000 +b=6370000 +units=m +no_defs') RETURNING srid"\
+                .format(ncfile['WRF'].TRUELAT1, ncfile['WRF'].TRUELAT2, ncfile['WRF'].CEN_LAT, ncfile['WRF'].STAND_LON)
 
     # Info
     logger.debug("Creating a new WRF SRID for this domain: {}".format(srid_sql))
@@ -49,5 +60,3 @@ def create_wrf_srid(windb2_instance, ncfile):
     srid = windb2_instance.curs.fetchone()[0]
     print("Newly created SRID: ", srid)
     return srid
-
-
