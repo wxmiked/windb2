f8578ff3d9268ef691203b9db604c151d2203d4f
Mike Dvorak <mike@wxtactics.com>
2020-12-27 21:07:15 +0000

    Resolve "As WinDB2, I need to insert GFS variables into CF Convention names, so that we can remove per-forecast differeces in naming"

--- windb2/model/gfs/util.py
+++ windb2/model/gfs/util.py
@@ -2,6 +2,6 @@ import numpy
 
 def center_coords_on_prime_meridian(longitude):
     """Takes a numpy array of longitude from 0 to 360 degrees and makes it from -180 to 180 degrees"""
-    longitude[longitude > 180] = 180 - longitude[longitude > 180]
+    longitude[longitude > 180] = longitude[longitude > 180] - 360
 
     return longitude
